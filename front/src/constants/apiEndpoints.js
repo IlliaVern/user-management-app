e6fcab81d3e04572e55f9ece39af09b3356a4114
Illia Vernygora <illiavernigora@gmail.com>
2020-06-05 14:49:49 +0300

    fixed add at apiEndpoints.js

--- front/src/constants/apiEndpoints.js
+++ front/src/constants/apiEndpoints.js
@@ -1,7 +1,7 @@
 export default {
     users: {
         read: `${process.env.VUE_APP_API_URL}/users`,
-        add: `${process.env.VUE_APP_API_URL}/users/add`,
+        add: `${process.env.VUE_APP_API_URL}/users`,
         delete: `${process.env.VUE_APP_API_URL}/users`,
         update: (id) => `${process.env.VUE_APP_API_URL}/users/${id}`,
         findById: (id) => `${process.env.VUE_APP_API_URL}/users/${id}`
